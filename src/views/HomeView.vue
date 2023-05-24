68ed8bb5f67f15a2f8fa7c899ade3a7463b1f1c6
Federico <fromani@matchupglobal.com>
2023-05-24 17:23:08 -0300

    Fix styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,10 @@ import { fetchBySubregion } from '@/composables/countriesFetch'
 import type { Country } from '@/models';
 import { computed, ref } from 'vue';
 const results = ref<Country[]>([])
+const isCalled = ref(false)
 const getCountries = async (subregion?: string) => {
   results.value = await fetchBySubregion(subregion)
+  isCalled.value = true
 }
 const countryNames = computed<string>(() => results.value.map(country => country.name.common).sort().join(', '))
 </script>
@@ -16,8 +18,8 @@ const countryNames = computed<string>(() => results.value.map(country => country
     <v-form class="my-4">
       <v-row>
         <v-col cols="12" md="6">
-          <v-btn class="ma-4" color="success" @click="getCountries()">Interceptar Sin Error</v-btn>
-          <v-btn color="error" @click="getCountries('Western Antarctica')">Interceptar Con Error</v-btn>
+          <v-btn class="ma-2" color="success" @click="getCountries()">Interceptar Sin Error</v-btn>
+          <v-btn class="ma-2" color="error" @click="getCountries('Western Antarctica')">Interceptar Con Error</v-btn>
         </v-col>
       </v-row>
     </v-form>
@@ -29,7 +31,7 @@ const countryNames = computed<string>(() => results.value.map(country => country
       variant="tonal"
     ></v-alert>
     <v-alert
-      v-else
+      v-else-if="isCalled"
       type="error"
       title="PAISES EN ANTÁRTIDA OCCIDENTAL"
       text="No hay paises en esta región"
