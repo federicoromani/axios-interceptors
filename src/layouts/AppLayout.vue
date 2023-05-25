d786f5458a333a1a9edd4a5ce3c4ca1553397c74
Federico <fromani@matchupglobal.com>
2023-05-25 15:34:11 -0300

    Fix styles

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import {VApp, VMain, VContainer} from 'vuetify/components'
+import { VApp, VMain, VContainer, VToolbar, VToolbarTitle } from 'vuetify/components'
 import ToastMessage from '@/common/components/ToastMessage.vue'
-import ApiLoader from '@/common/components/ApiLoader.vue';
+import ApiLoader from '@/common/components/ApiLoader.vue'
 </script>
 <template>
   <v-app id="new-app" class="w-100">
+    <v-toolbar color="blue-grey-darken-2" dark>
+      <v-toolbar-title> INTERCEPTORES </v-toolbar-title>
+    </v-toolbar>
     <v-main class="bg-grey-lighten-5">
       <v-container class="bg-transparent" fluid style="height: 100%">
         <router-view></router-view>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,34 @@
 <script setup lang="ts">
-import {VContainer, VForm, VRow, VCol, VBtn, VAlert} from 'vuetify/components'
+import { VContainer, VForm, VRow, VCol, VBtn, VAlert } from 'vuetify/components'
 import { fetchBySubregion } from '@/composables/countriesFetch'
-import type { Country } from '@/models';
-import { computed, ref } from 'vue';
+import type { Country } from '@/models'
+import { computed, ref } from 'vue'
+import { useDisplay } from 'vuetify'
 const results = ref<Country[]>([])
 const isCalled = ref(false)
 const getCountries = async (subregion?: string) => {
   results.value = await fetchBySubregion(subregion)
   isCalled.value = true
 }
-const countryNames = computed<string>(() => results.value.map(country => country.name.common).sort().join(', '))
+const countryNames = computed<string>(() =>
+  results.value
+    .map((country) => country.name.common)
+    .sort()
+    .join(', ')
+)
+const breakpoint = ref(useDisplay())
 </script>
 <template>
   <v-container fluid>
-    <div class="text-h5 text-uppercase">INTERCEPTORES</div>
-    <div class="text-subtitle-1 text-grey">CON MENSAJES EMERGENTES</div>
     <v-form class="my-4">
-      <v-row>
-        <v-col cols="12" md="6">
+      <v-row justify="center">
+        <v-col :cols="breakpoint.mdAndDown ? 12 : 3" class="d-flex justify-center">
           <v-btn class="ma-2" color="success" @click="getCountries()">Interceptar Sin Error</v-btn>
-          <v-btn class="ma-2" color="error" @click="getCountries('Western Antarctica')">Interceptar Con Error</v-btn>
+        </v-col>
+        <v-col :cols="breakpoint.mdAndDown ? 12 : 3" class="d-flex justify-center">
+          <v-btn class="ma-2" color="error" @click="getCountries('Western Antarctica')"
+            >Interceptar Con Error</v-btn
+          >
         </v-col>
       </v-row>
     </v-form>
